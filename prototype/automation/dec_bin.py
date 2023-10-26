d89d01bab4a2c181f1c1bafeaf235dc52a3b41d3
omikami747 <omkarkamath747@gmail.com>
2023-10-26 11:47:04 +0530

    fixed [1,2) input value issue for fisqr

--- prototype/automation/dec_bin.py
+++ prototype/automation/dec_bin.py
@@ -10,13 +10,17 @@ def float_to_ieee754(float_number):
 input_file = '../test/fisqr_decimal.txt'
 output_file = 'fisqr_binary.txt'
 output_file_1 = 'expected_result.txt'
+
 try:
     with open(input_file, 'r') as input_file, open(output_file, 'w') as output_file, open(output_file_1, 'w') as output_file_1:
         for line in input_file:
+            #print(line)
             try:
                 float_number = float(line.strip())
                 ieee754_representation = float_to_ieee754(float_number)
-                binary_string = bin(ieee754_representation)[2:28].zfill(27)  # Convert to binary string
+                # print(bin(ieee754_representation))
+                # print(bin(ieee754_representation)[2:].zfill(32)[0:26])
+                binary_string = bin(ieee754_representation)[2:].zfill(32)[0:27]  # Convert to binary string
                 output_file.write(binary_string + '\n')
                 output_file_1.write(str(1/math.sqrt(float_number))+'\n')
             except ValueError:
